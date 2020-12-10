b6d377f8bc4301b82344a39c727377965c06bb3f
sangosteve <stevesango9@gmail.com>
2020-12-10 17:14:31 +0200

    modified the push to cart feature to increase on ly the quantity in the cart for a product that already exists

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,7 +6,6 @@ import {Trash2} from 'react-feather';
 
 function Cart(){
     const [cart,setCart] = useContext(CartContext);
-    const [cartTotal,setCartTotal] = useState(0)
     const [productQty,setProdQty]=useState(0);
     function removeFromCart(productToRemove){
         setCart(cart.filter(product=>product!==productToRemove))
